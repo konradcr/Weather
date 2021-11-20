e4c5a7848033eda5ebea3532c0f650ddadc0e29b
konradcr <63312007+konradcr@users.noreply.github.com>
2021-11-20 16:50:12 +0100

    Creating a day/night cycle

--- Weather/Cloud.swift
+++ Weather/Cloud.swift
@@ -11,12 +11,12 @@ class Cloud {
     enum Thickness: CaseIterable {
         case none, thin, light, regular, thick, ultra
     }
-    
+
     var position: CGPoint
     let imageNumber: Int
     let speed = Double.random(in: 4...12)
     let scale: Double
-    
+
     init(imageNumber: Int, scale: Double) {
         self.imageNumber = imageNumber
         self.scale = scale
